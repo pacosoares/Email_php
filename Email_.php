4cc7ca9f9162a969c2d3b727d4a250dc5bec7cd7
Paco <soares-pascoal@bol.com.br>
2015-02-11 15:40:37 -0200

    Send email to enyone.
    Code to send emails to anyone with PHP.

--- Email_.php
+++ Email_.php
@@ -3,125 +3,93 @@
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
+Autor: Pascoal J.M.S.
+Date: 2014-02-11
 -->
 <!doctype html>
         <html>
-            <head>
+            <head>            
              <meta http-equiv="Content-Type" content="text/html" CHARSET = "UTF-8" />
              <link rel="stylesheet" type="text/css" href="CSS/All.css"/>
             </head>
             <div id="email"> <body>
                 <?php 
-                //session_start();
-                //echo $_SESSION['nome'];
                include("Connect.php");
-                //include 'Class/Validacao.class.php'; 
-                 //include 'Model/Insert.class.php';
+               
 
+			                    if(isset($_POST['Cadastrar'])){
 
-                    if(isset($_POST['Cadastrar'])){
+			                                        function email(){
+			                                                                                 
+			                                        $titulo=$_POST['titulo'];
+			                                        $destino = $_REQUEST['email_send'];
+			                                        $message = $_POST['message'];
+													$youremail = $_POST['your_email'];
+													$name = $_POST['name'];
+			                                        $headers = "Content-Yype:text/html; charset = UTF-8\n";
+			                                        $headers .= "From :".$youremail."\n";
+			                                        $headers .= "X-Sender :<".$youremail.">\n";
+			                                        $subject = "$titulo";
+			                                        $message = "$message \n\n\n";
+			                                        
 
-                                        function email(){
-                                            
-                                       
-                                        $titulo=$_POST['titulo'];
-                                        $destino = $_REQUEST['destinatario'];
-                                        $mensagem = $_POST['mensagem'];
-										$nome = $_POST['nome'];
-                                        //echo $mensagem1;
-                                        //echo $comunidade.$titulo.$destino;
- 
+			                                 if(mail($destino,$titulo,$message,$headers)){
+													
 
-
-
-
-                                        $headers = "Content-Yype:text/html; charset = UTF-8\n";
-                                        $headers .= "From :jornal@pascoal.esy.es\n";
-                                        $headers .= "X-Sender :<jornal@pascoal.esy.es>\n";
-                                        $subject = "$titulo";
-                                        $mensagem = "$mensagem \n\n\n";
-                                        //$mensagem .= "Este email é automático, por favor, não responde-lo \nAtt.$nome";
-                                        ///$mensagem .= "http://pascoal.esy.es/Sonda/CadastroOk.php?nome=$nome&id=$id";
-                                        
-                                        //$link = "<a href= '' > link </a>";
-                                        //$mensagem = "Click no link para confirmar cadastro.";
-
-                                        if(mail($destino,$titulo,$mensagem,$headers)){
-										
-
-?>
-			<script>
-			alert("Ação inserida com sucesso!")
-			window.location="<?$_SERVER["SERVER_ADDR"];?>http://pascoal.esy.es/Jornal/Email_.php"
-			</script>
-			<?php
-			
-		}else{		
 			?>
-			<script>
-			alert("Erro ao inserir ação")
-			window.location="<?$_SERVER["SERVER_ADDR"];?>http://pascoal.esy.es/Jornal/Email_.php"
-			</script>
-			<?php
-		}
-                                        }
-//	//}else
-//		{
-//		echo "<script>alert(\"Por favor, preencha os campos corretamente.\")</script>";
-//		echo "<script>
-//		history.back();   
-//		</script>  
-//				";
-//		
-//		
-//	
-//	}	
-//                                        }else{
-//                                                echo 'email não enviado';
-//                                               // echo $headers.$nome.$email;
-//                                                //echo $nome.$email;
-//                                        }
-//                                        }
+						<script>
+						alert("Ação inserida com sucesso!")
+						window.location="<?$_SERVER["SERVER_ADDR"];?>http://pascoal.esy.es/Jornal/Email_.php"
+						</script>
+						<?php
+						
+					}else{		
+						?>
+						<script>
+						alert("Erro ao inserir ação")
+						window.location="<?$_SERVER["SERVER_ADDR"];?>http://pascoal.esy.es/Jornal/Email_.php"
+						</script>
+						<?php
+					}
+			                                        }
 
-                                       email();
-
- 
-                  
-
-
-
-
-                                }
+			                                       email();
+			                                       
+			     
+			                                }
                                 
                                 ?>
-            <center>
+                        <center>
                         <form name="Cadastrar" method="post" action="">
-                          <h2>Enviar email ao administrador</h2>
+                          <label>
+                          <h2>Send one e-mail to Anyone.</h2>
                           </label><br><br><br>                            
-                            <label for="titulo">Titulo</label>
-                            <input type="text" name="titulo" style="margin-left:66px;">
+                            <label for="titulo">Title</label>
+                            <input type="text" name="titulo" style="margin-left:78px; width:430px;">
                             <br>
-							<label for="titulo">Destinatario</label>
-                            <input type="text" name="destinatario"><br>
-							<label for="titulo">Mensagem</label>
-                            <input type="text" name="mensagem" style="margin-left:16px;"><br>
-							<label for="titulo" name = "nome"> Nome</label>
-                            <input name ="nome" type="text" style="margin-left: 64px; margin-top: 0px;"></text><br><br><br>
+							<label for="titulo">Email_Send</label>
+                            <input type="text" name="email_send" style="margin-left:3px; width:430px;"><br>	
+                            
+                            <label for="titulo">Your Email</label>
+                            <input type="text" name="your_email" style="margin-left:14px; width:430px;"><br>								
+							<label for="titulo" name = "name"> Name</label>
+                            <input name ="name" type="text" style="margin-left: 65px; margin-top: 0px; width:430px;"></text><br><br>
+                            
+                            <label for="titulo">Message</label>
+                            <textarea name="message" style="margin-left:38px; width:430px;" cols="1" rows="5"> </textarea><br>                            <br><br>
                             <input type="hidden" name="Cadastrar"/>
                             <center>
-                            <input type="submit" name="OK" value="Enviar"/></center>
+                            <input type="submit" name="OK" value="SEND_EMAIL"/></center>
                         </form>
                         
                         
-                        <?php
-
-
-        //$query = $mysql_query("INSERT INTO `Start_emails`(`Id`, `Nome`, `Email`) VALUES ('','$nome' ,'$email,''");
-
-
-
-                        ?>
-                    </body>
+                       
+                      </body>
+                  
                     </div>
+       <center> <footer>Solutions in PHP</footer></center>
+
+
+       
         </html>
         
